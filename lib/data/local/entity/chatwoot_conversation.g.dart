d2546e4191c65a59f4321a3dca8a11df111d902d
Randi Julias <48124139+jaluowen@users.noreply.github.com>
2025-02-26 20:39:59 +0700

    Revert "Update chatwoot_conversation.g.dart"
    This reverts commit d4255f2c7d3f3fd0986f65018bc279c13a956ad7.

--- lib/data/local/entity/chatwoot_conversation.g.dart
+++ lib/data/local/entity/chatwoot_conversation.g.dart
@@ -21,14 +21,13 @@ class ChatwootConversationAdapter extends TypeAdapter<ChatwootConversation> {
       inboxId: fields[1] as int,
       messages: (fields[2] as List).cast<ChatwootMessage>(),
       contact: fields[3] as ChatwootContact,
-      status: fields[4] as String,
     );
   }
 
   @override
   void write(BinaryWriter writer, ChatwootConversation obj) {
     writer
-      ..writeByte(5)
+      ..writeByte(4)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
@@ -36,9 +35,7 @@ class ChatwootConversationAdapter extends TypeAdapter<ChatwootConversation> {
       ..writeByte(2)
       ..write(obj.messages)
       ..writeByte(3)
-      ..write(obj.contact)
-      ..writeByte(4)
-      ..write(obj.status);
+      ..write(obj.contact);
   }
 
   @override
@@ -59,14 +56,14 @@ class ChatwootConversationAdapter extends TypeAdapter<ChatwootConversation> {
 ChatwootConversation _$ChatwootConversationFromJson(
         Map<String, dynamic> json) =>
     ChatwootConversation(
-        id: json['id'] as int,
-        inboxId: json['inbox_id'] as int,
-        messages: (json['messages'] as List<dynamic>)
-            .map((e) => ChatwootMessage.fromJson(e as Map<String, dynamic>))
-            .toList(),
-        contact:
-            ChatwootContact.fromJson(json['contact'] as Map<String, dynamic>),
-        status: json['status'] as String);
+      id: json['id'] as int,
+      inboxId: json['inbox_id'] as int,
+      messages: (json['messages'] as List<dynamic>)
+          .map((e) => ChatwootMessage.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      contact:
+          ChatwootContact.fromJson(json['contact'] as Map<String, dynamic>),
+    );
 
 Map<String, dynamic> _$ChatwootConversationToJson(
         ChatwootConversation instance) =>
@@ -75,5 +72,4 @@ Map<String, dynamic> _$ChatwootConversationToJson(
       'inbox_id': instance.inboxId,
       'messages': instance.messages.map((e) => e.toJson()).toList(),
       'contact': instance.contact.toJson(),
-      'status': instance.status,
     };
